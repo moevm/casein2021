3c80f1ef8682f316b2d8aba2aa39b78e2a46eb73
Dmitry Ivanov <darcenrall@gmail.com>
2021-03-20 18:24:33 +0300

    add course/task creating/updating

--- src/app/db_models.py
+++ src/app/db_models.py
@@ -1,5 +1,8 @@
 import mongoengine as me
 
+class NoRequiredField(Exception):
+    pass
+
 
 class User(me.Document):
     _id = me.StringField(primary_key=True)
@@ -9,17 +12,42 @@ class User(me.Document):
     progress  = me.DictField() 
 
 
-class Task(me.Document):
+class Task(me.EmbeddedDocument):
     _id = me.StringField(primary_key=True)
     name = me.StringField()
     condition = me.StringField()
     task_type = me.IntField()
-    some_info  = me.DictField() 
+    check = me.DictField() 
+
+    @staticmethod
+    def from_object(task_object):
+        for key in ('_id', 'name', 'condition', 'task_type', 'check'):
+            if key not in task_object:
+                raise NoRequiredField(f'Отсутствует необходимое поля задания: {key}')
+
+        return Task(_id=task_object.get('_id'),
+                name=task_object.get('name'),
+                condition=task_object.get('condition'),
+                task_type=task_object.get('task_type'),
+                check=task_object.get('check'))
 
 
 class Course(me.Document):
     _id = me.StringField(primary_key=True)
     name = me.StringField()
-    tasks = me.ListField(me.ReferenceField(Task))
+    tasks = me.EmbeddedDocumentListField(Task)
     users = me.ListField(me.ReferenceField(User))
     some_info = me.DictField() 
+
+    @staticmethod
+    def from_object(course_object):
+        for key in ('_id', 'name', 'tasks'):
+            if key not in course_object:
+                raise NoRequiredField(f'Отсутствует необходимое поле курса: {key}')
+
+        return Course(_id=course_object.get('_id'),
+                name=course_object.get('name'),
+                tasks=[Task.from_object(task) for task in course_object['tasks']])
+
+
+def get_course(course_id): return Course.objects(_id=course_id).first()
--- src/app/routes/course.py
+++ src/app/routes/course.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
-from app.db_models import Course
+from flask import Blueprint, request
+from app.db_models import Course, Task, get_course
 
 
 bp = Blueprint('course', __name__, url_prefix='/course')
@@ -9,13 +9,31 @@ bp = Blueprint('course', __name__, url_prefix='/course')
 def course_index():
     return str(len(Course.objects))
 
+@bp.route('/create')
+def course_create():
+    return 'Создание курса'
 
-@bp.route('/<int:course_id>')
+
+@bp.route('/<course_id>')
 def course_page(course_id):
-    return f'Get {course_id}: {Course.objects(_id=course_id).first()}'
+    course = get_course(course_id)
+    return course.to_json() if course else (f'Курс {course_id} не найден', 404)
 
 
-@bp.route('/update/<int:course_id>')
+@bp.route('/update/<course_id>', methods=['GET', 'POST'])
 def course_update(course_id):
-    return f'Update {course_id}: {Course.objects(_id=course_id).first()}'
+    """
+    GET - страница редактирования курса (== страница создания курса с заполненными полями)
+    POST - обновление курса
+    """
+    if request.method == 'POST':
+        return get_course(course_id)
+    else:
+        # пример словаря с клиента
+        obj = {'_id': course_id, 'name': 'name', 'tasks': [dict(_id='_id1', name='name', condition='condition', task_type=1, check={}),
+            dict(_id='_id2', name='name', condition='condition', task_type=1, check={})]
+        }
+        course = Course.from_object(obj)
+        course.save()
+        return course.to_json()
 
